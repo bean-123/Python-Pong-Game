7f287cd078f4e9643205a809f351db193d24ced4
Amy <amy.platt@hotmail.com>
2026-02-09 13:52:54 +0200

    Add movement of the paddles

--- solution.py
+++ solution.py
@@ -27,6 +27,13 @@ class Paddle:
     def draw(self, win): #Drawing the paddles as rectangles
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
+    #Moving the paddle on Y axel, connection with handle_paddle_movement
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
 #Changing the color of the Display
 def draw(win, paddles):
     win.fill(BLACK)
@@ -35,6 +42,18 @@ def draw(win, paddles):
         paddle.draw(win)
 
     pygame.display.update() #Need to update the display
+
+#Defining the keys
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w]:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s]:
+        left_paddle.move(up=False)
+    
+    if keys[pygame.K_UP]:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN]:
+        right_paddle.move(up=False)
 
 def main():
     run = True
@@ -52,6 +71,10 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
+
     pygame.quit()
 
 if __name__ == '__main__':
